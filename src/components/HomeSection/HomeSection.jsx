8253c5107bc6d0f23fc96dfc229dd70de4811b06
Amritchandra <amritchandrakumar123@gmail.com>
2023-12-20 16:10:15 +0530

    A

---
+++ src/components/HomeSection/HomeSection.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import "./HomeSection.css"
+import Typewriter from 'typewriter-effect';
+import MyResume from "../Navbar/MyResume.pdf"
+
+function HomeSection() {
+    const downloadHandle = () => {
+        const link = document.createElement("a");
+        link.download = "Amrit-Chandra-Kumar-Resume.pdf";
+        link.href = MyResume;
+        link.setAttribute("class", "resume-link-2")
+        link.click();
+
+        window.open(
+            "https://drive.google.com/file/d/1c9DoRtNiSPjUVmBKpzvHMvawOU6Y6rc8/view?usp=sharing",
+            "_blank"
+        );
+    };
+
+    const githubHandle = () => {
+        window.open(
+            "https://github.com/amritchandra",
+            "_blank"
+        );
+    };
+    return (
+        <div id='home'>
+            <div id='left_home'>
+                <div>
+                    <h1>Hi, I am</h1>
+                    <h1 id="user-detail-name">Amrit Chandra Kumar</h1>
+                    {/* <h3>I am a Full Stack Web Developer</h3> */}
+                    <h3>
+                        I am a
+                        <span>
+                            <Typewriter
+                                options={{
+                                    strings: "Full Stack Web Developer",
+                                    autoStart: true,
+                                    loop: true,
+                                }}
+                            />
+                        </span>
+                    </h3>
+                   
+                    <p> Specialized in MERN stack, like to write code clean and optimized, experienced inexecuting solo and collaborative projects. Possesses
+                       strong problem-solving abilities, keen attention to detail,and a passion for crafting exceptional web solutions.With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and want to achieve great things. You can reach me at <a href="#contact" className='contactMe' >Contact Me</a></p>
+                    <div className='home_button'>
+                        <button id="resume-button-2"
+                            onClick={downloadHandle}>
+                            Resume
+                        </button>
+                        <button onClick={githubHandle} >
+                            GitHub
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div id='right_home' >
+                <img src="/Images/Amrit_image.jpg" alt="" className="home-img" />
+            </div>
+        </div>
+    )
+}
+
+export default HomeSection
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,109 @@
+import React from 'react'
+import greenParadise from "./projectImages/Gemgarden.png"
+import woodFans from "./projectImages/Pharmeasy.png"
+import bewakoof from "./projectImages/Pharmeasy.png"
+import myPortfolio from "./projectImages/Gemgarden.png"
+import "./Project.css"
+import { motion } from "framer-motion"
+
+const data = [
+    {
+        img: woodFans,
+        projectTitle: "Pharmeasy Clone",
+        techStack: ["HTML", "CSS", "JavaScript"],
+        description: " PharmEasy is a consumer healthcare “super app” that provides consumers with on-demand, home delivered access to a wide range of prescription, OTC pharmaceutical, other consumer healthcare products, comprehensive diagnostic test services, and teleconsultations thereby serving their healthcare needs.",
+        githubLink: "https://github.com/Amritchandra/Pharmeasy-clone",
+        appLink: "https://magnificent-pie-033e94.netlify.app/"
+    },
+    {
+        img: greenParadise,
+        projectTitle: "Gem Garden Jewellary ",
+        techStack: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+        description: "Explore the exquisite world of Gem Garden, an online jewelry website where you can effortlessly purchase the jewelry of your choice from the comfort of your home.",
+        githubLink: "https://github.com/Amritchandra/Gem-Garden-Project",
+        appLink: "https://gem-garden-jewelry-store.vercel.app/"
+    },
+    {
+        img: bewakoof,
+        projectTitle: "Pharmeasy Clone",
+        techStack: ["HTML", "CSS", "JavaScript",],
+        description: " PharmEasy is a consumer healthcare “super app” that provides consumers with on-demand, home delivered access to a wide range of prescription, OTC pharmaceutical, other consumer healthcare products, comprehensive diagnostic test services, and teleconsultations thereby serving their healthcare needs.",
+        githubLink: "https://github.com/Amritchandra/Pharmeasy-clone",
+        appLink: "https://magnificent-pie-033e94.netlify.app/"
+    }, {
+        img: myPortfolio,
+        projectTitle: "Gem Garden Jewellary ",
+        techStack: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+        description: "Explore the exquisite world of Gem Garden, an online jewelry website where you can effortlessly purchase the jewelry of your choice from the comfort of your home.",
+        githubLink: "https://github.com/Amritchandra/Gem-Garden-Project",
+        appLink: "https://gem-garden-jewelry-store.vercel.app/"
+    }
+]
+
+
+
+
+function Projects() {
+    return (
+        <div id='projects'>
+            <h1>Projects</h1>
+
+            {
+                data.map((e, index) => {
+                    return (
+
+                        index % 2 == 0 ? (
+                            <div className="project-card proCard1" key={index}>
+                                <div className='projectImg'>
+                                    <img src={e.img} alt="image" />
+                                </div>
+                                <div className='projectDetails'>
+                                    <h2 className="project-title">{index + 1 + ". " + e.projectTitle}</h2>
+                                    <div className="project-tech-stack">
+                                        {
+                                            e.techStack.map((ele, ind) => {
+                                                return (
+                                                    <span key={ind}>{ele}</span>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    <p className="project-description">{e.description}</p>
+                                    <div className='projectButtons'>
+                                        <a href={e.githubLink} target="_blank" className="project-github-link">View Code</a>
+                                        <a href={e.appLink} target="_blank" className="project-deployed-link">View Live App</a>
+                                    </div>
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="project-card proCard2" key={index}>
+                                <div className='projectDetails'>
+                                    <h2 className="project-title">{index + 1 + ". " + e.projectTitle}</h2>
+                                    <div className="project-tech-stack">
+                                        {
+                                            e.techStack.map((ele, ind) => {
+                                                return (
+                                                    <span key={ind}>{ele}</span>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    <p className="project-description">{e.description}</p>
+                                    <div className='projectButtons'>
+                                        <a href={e.githubLink} target="_blank" className="project-github-link">View Code</a>
+                                        <a href={e.appLink} target="_blank" className="project-deployed-link">View Live App</a>
+                                    </div>
+                                </div>
+                                <div className='projectImg'>
+                                    <img src={e.img} alt="" />
+                                </div>
+                            </div>
+                        )
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default Projects
